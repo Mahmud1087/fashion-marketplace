4bba0f2027ded7d3bc91c9008163c553fef41c4a
Mahmud1087 <abdulazeezm578@gmail.com>
2024-10-13 22:50:07 +0100

    edit

--- src/app/(general)/page.tsx
+++ src/app/(general)/page.tsx
@@ -6,6 +6,10 @@ import shield from '~/public/shield.png';
 import clock from '~/public/24-hours.png';
 import Image from 'next/image';
 import { Button } from '@/components/ui/button';
+import menCategory from '~/public/men-category.png';
+import womenCategory from '~/public/women-category.png';
+import kidsCategory from '~/public/kids-category.png';
+import heroImg from '~/public/heroimg1.png';
 // import { ArrowRight } from 'lucide-react';
 
 const benefits = [
@@ -36,31 +40,40 @@ const Home = () => {
     <div className='overflow-hidden'>
       {/* <Hero /> */}
 
-      <section className='relative w-screen pt-32 pb-56 flex items-center justify-center flex-col bg-slate-50 shadow-lg'>
-        <p className='text-center text-sm text-orange-700 mb-3 font-medium'>
-          WELCOME TO FASHMALL
-        </p>
-        <h1 className='text-center text-2xl text-black font-bold mb-3 sm:text-3xl lg:text-4xl'>
-          Discover Fashion That <br />
-          <span className='text-primary'> Defines You</span>
-        </h1>
-        <p className='text-base text-gray-500 text-center mb-10 w-full mx-auto sm:w-3/5 lg:w-1/2 px-5'>
-          Shop the latest trends, find unique pieces, and support independent
-          sellers—all in one place. Join the marketplace where fashion meets
-          convenience.
-        </p>
-        <div className='flex items-center justify-center gap-5 w-full'>
-          <Button
-            variant='outline'
-            className='text-base flex gap-4 group transition-all shadow-md'
-          >
-            Start Shopping
-          </Button>
-          <Button className='text-base flex gap-4 group transition-all bg-gray-800 hover:bg-gray-900 shadow-md'>
-            Become a seller
-          </Button>
-        </div>
+      <section className='bg-slate-100 shadow-lg'>
+        <MaxWidthWrapper className='relative w-screen pt-32 pb-56  flex items-center'>
+          <aside className='flex items-center justify-center flex-col sm:items-start'>
+            <p className='text-center sm:text-start text-sm text-orange-700 mb-3 font-medium'>
+              WELCOME TO FASHMALL
+            </p>
+            <h1 className='text-center sm:text-start text-2xl text-black font-bold mb-3 sm:text-3xl lg:text-4xl'>
+              Discover Fashion That <br />
+              <span className='text-primary'> Defines You</span>
+            </h1>
+            <p className='text-base text-gray-500 text-center sm:text-start mb-10 w-full mx-auto sm:mx-0 sm:px-0 sm:w-3/5 lg:w-1/2 px-5'>
+              Shop the latest trends, find unique pieces, and support
+              independent sellers—all in one place. Join the marketplace where
+              fashion meets convenience.
+            </p>
+            <div className='flex items-center justify-center gap-5 w-full sm:justify-start'>
+              <Button
+                variant='outline'
+                className='text-base flex gap-4 group transition-all shadow-md'
+              >
+                Start Shopping
+              </Button>
+              <Button className='text-base flex gap-4 group transition-all bg-gray-800 hover:bg-gray-900 shadow-md'>
+                Become a seller
+              </Button>
+            </div>
+          </aside>
+          <div className='hidden absolute irregular-shape right-0 top-5 bg-black/25 sm:block'></div>
+          <div className='absolute right-0 top-0 hidden sm:block'>
+            <Image src={heroImg} alt='hero image' />
+          </div>
+        </MaxWidthWrapper>
       </section>
+
       <MaxWidthWrapper className='rounded-lg bg-white shadow-2xl p-8 mb-14 -mt-14 relative grid gap-10 sm:grid-cols-2 sm:-mt-24 lg:grid-cols-4 lg:gap-0 lg:-mt-10'>
         {benefits.map((benefit, index) => {
           return (
@@ -81,6 +94,48 @@ const Home = () => {
             </div>
           );
         })}
+      </MaxWidthWrapper>
+
+      <MaxWidthWrapper className='my-44'>
+        <section className='grid gap-6 sm:grid-cols-4'>
+          <div className='w-full h-72 bg-black/20 rounded-xl relative transition-all group'>
+            {/* <h1 className='absolute -left-3 top-1/2 -translate-y-1/2 -rotate-90 text-4xl text-primary cursor-pointer group-hover:rotate-0 group-hover:left-1/2 group-hover:-translate-x-1/2 transition-all'>
+              Men
+            </h1> */}
+            <div className='w-full h-full'>
+              <Image
+                src={menCategory}
+                alt='man image'
+                className='w-full h-full'
+              />
+            </div>
+          </div>
+          <div className='w-full h-72 bg-black/20 rounded-xl relative transition-all group'>
+            {/* <h1 className='absolute -left-3 top-1/2 -translate-y-1/2 -rotate-90 text-4xl text-primary cursor-pointer group-hover:rotate-0 group-hover:left-1/2 group-hover:-translate-x-1/2 transition-all'>
+              Women
+            </h1> */}
+            <div className='w-full h-full'>
+              <Image
+                src={womenCategory}
+                alt='man image'
+                className='w-full h-full'
+              />
+            </div>
+          </div>
+          <div className='w-full h-72 bg-black/20 rounded-xl relative transition-all group'>
+            {/* <h1 className='absolute -left-3 top-1/2 -translate-y-1/2 -rotate-90 text-4xl text-primary cursor-pointer group-hover:rotate-0 group-hover:left-1/2 group-hover:-translate-x-1/2 transition-all'>
+              Kids
+            </h1> */}
+            <div className='w-full h-full'>
+              <Image
+                src={kidsCategory}
+                alt='man image'
+                className='w-full h-full'
+              />
+            </div>
+          </div>
+          <div className='w-full h-56'>Top Categories</div>
+        </section>
       </MaxWidthWrapper>
     </div>
   );
